758f604fe14a1cbff51ed21fd7e261b22d5d425b
El Wafi Cherkaoui <elwaficherkaoui@gmail.com>
2025-01-31 13:43:03 +0100

    Fix : Content Security Policy && CORS

--- server/app.ts
+++ server/app.ts
@@ -1,18 +1,50 @@
 import express from "express"
 import dotenv from "dotenv"
 import { Server } from "socket.io"
+import cors from 'cors'
+
+import path from "path"
+import { fileURLToPath } from "url";
 
 dotenv.config()
 
 const PORT = process.env.BACKEND_PORT
 const app = express()
-const http_server = app.listen(PORT, ()=>console.log("server is running on: ", PORT))
+app.use(
+    cors({
+        origin: process.env.FRONTEND_SERVER || "http://localhost:4173",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Content-Type"],
+    })
+);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, '../dist')));
 
-const io = new Server(http_server, {
-    cors : {
-        origin : process.env.FRONTEND_SERVER
-    }
+app.use((_req, res, next) => {
+    res.setHeader(
+        "Content-Security-Policy", `default-src 'self'; script-src 'self' 'unsafe-inline'; img-src 'self' ${process.env.FRONTEND_SERVER} data:; connect-src 'self' ${process.env.FRONTEND_SERVER};`
+    );
+    next();
+});
+app.get("/", (_req, res) => {    
+    res.send("websocket server is on");
+});
+const http_server = app.listen(PORT, ()=>{
+    console.log("server is running on: ", PORT)
 })
+const io = new Server(http_server, {
+    cors: {
+        // origin: [process.env.FRONTEND_SERVER || "http://localhost:4173"], 
+        origin: "*", 
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Content-Type"],
+        credentials: true
+    },
+    transports: ["websocket"],
+    pingInterval: 60000, 
+    pingTimeout: 50000, 
+});
 type Message_Type = {
     sender: string
     text: string
@@ -29,7 +61,10 @@ const rooms : Rooms_type[] = [
 
 io.on("connection", (cnn)=>{
     console.log("someone connected to server")
-    
+    cnn.on("disconnect", () => {
+        console.log("user disconnected", cnn.id);
+    });
+
     cnn.on("user_info", (user_info)=>{
         console.log("user connected: ", user_info);
         cnn.join(user_info.roomID)
@@ -55,3 +90,6 @@ io.on("connection", (cnn)=>{
 
 
 })
+app.get('*', (_req, res) => { 
+    res.sendFile(path.join(__dirname, '../dist', 'index.html'))
+});
--- src/App.tsx
+++ src/App.tsx
@@ -13,14 +13,20 @@ export type UserType = {
 function App() {
   const [socket, set_socket] = useState <SocketType | null> (null)
   const [userInfo, set_userInfo] = useState<UserType | null>(null)
+  console.log("socket:", socket);
   
   useEffect(()=>{
     try {
       if(!userInfo){
         return
       }
-      const socket_inst: SocketType = io(import.meta.env.VITE_BACKEND_SERVER);
+      const socket_inst: SocketType = io(import.meta.env.VITE_BACKEND_SERVER, {
+        transports: ["websocket"],
+      });
+      console.log("socket_inst:", socket_inst);
+      
       socket_inst.on("connect", ()=>{
+        console.log('connected');
         socket_inst.emit("user_info", userInfo)
       })
       set_socket(socket_inst)
